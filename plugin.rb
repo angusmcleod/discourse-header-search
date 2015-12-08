ed78d20281b4cef0cf90b9ea8bb603d5b06bd8fe
Angus McLeod <angus@mcleod.org.au>
2015-12-08 12:12:08 -0500

    I dislike using the word 'permanent'

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
-# name: discourse-permanent-header-search
-# about: A Discourse plugin that makes the search bar always visible in the header
+# name: discourse-header-search
+# about: A Discourse plugin that makes the search bar visible in the header
 # version: 0.1
 # authors: Angus McLeod
 
