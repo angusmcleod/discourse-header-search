4086a6380d5426fe7a4a73f3e020dc4a2982425f
Angus McLeod <angus@mcleod.org.au>
2017-08-13 14:24:39 +0800

    Init theme

--- plugin.rb
+++
@@ -1,8 +0,0 @@
-# name: discourse-header-search
-# about: A Discourse plugin that makes the search bar visible in the header
-# version: 0.1
-# authors: Angus McLeod
-
-enabled_site_setting :override_extra_info
-
-register_asset 'stylesheets/header-search.scss', :desktop
